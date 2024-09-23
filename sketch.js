3d9590f06af5455c1af61f097d5a214abcbaa238
TIEN TRUONG <ttt21b@fsu.edu>
2024-09-23 00:58:24 -0400

    still need to do diameterSliderChange

--- sketch.js
+++ sketch.js
@@ -1,7 +1,45 @@
+let d = 60;
+let deg = 58;
+let outline = true;
+
+let gui;
+let diameterSlider, degSlider, outlineCheckbox;
+
+
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  strokeWeight(10);
+
+  gui = createGui();
+  diameterSlider = createSlider("diameter", 200, 10, 100, 20, 10, 100);
+  degSlider = createSlider("rotation", 200, 40, 100, 20, 0, 180);
+  degSlider.val = 23;
+  outlineCheckbox = createCheckbox("outline", 340, 10, 20, 20, true );
+
+
+  
 }
 
 function draw() {
   background(125);
+  drawGui();
+
+  if (outlineCheckbox.val) {
+  stroke("black");
+  } else {
+    noStroke();
+  }
+
+  circle (100,200,diameterSlider.val);
+
+  push();
+  translate(200,200);
+  rotate( radians(degSlider.val) );
+  rect (0,0,60,60);
+  pop();
+
+  noStroke();
+  text(diameterSlider.label,140, 25)
 }
